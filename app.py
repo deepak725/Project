e43987b15bcc01319164f1e0fea449cb3c5383ab
deepak725 <64675537+deepak725@users.noreply.github.com>
2021-03-06 00:04:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 CORS(app, support_credentials=True)
 
 # load the learner
-learn = load_learner(path='./models', file='model.pkl')
+learn = load_learner('./models','model.pkl')
 classes = learn.data.classes
 
 
 def predict_single(img_file):
-    'function to take image and return prediction'
-    prediction = learn.predict(open_image(img_file))
-    probs_list = prediction[2].numpy()
+
+	prediction = learn.predict(open_image(img_file))
+	probs_list = prediction[2].numpy()
     return {
         'category': classes[prediction[1].item()],
         'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
